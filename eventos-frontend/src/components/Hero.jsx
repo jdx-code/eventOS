8bce7ceeaae1127b7b144bb12c4950263becd3eb
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-22 15:05:20 +0530

    Working on Hero Section

--- eventos-frontend/src/components/Hero.jsx
+++ eventos-frontend/src/components/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = ()  => {
         <div className="w-full h-[500px]" id="hero">
             <div className="text-white flex flex-col text-center justify-center pt-40 mx-auto w-[800px]">
                 <h1 className="text-4xl">Welcome to EventOS</h1>
-                <p>WHAT'S OPEN SOURCE INDIA?</p>
-                <p>Open Source India (OSI) is an attempt by the team at Open Source For You magazine in partnership with India’s Open Source community and industry—to bring various stake-holders under one roof.</p>
-                <p>The mission of OSI is simple—to accelerate development and deployment of Open Source in India, and beyond.</p>
+                <p>WHAT IS EVENTOS?</p>
+                <p>EventOS is a platform to view, book and monitor events related to Computer Science and Engineering discipline that are held by various organisations throughout the year.</p>
+                <p>The mission of EventOS is to help Developers keep a track of the past and up-coming events accross the globe.</p>
             </div>            
         </div>
     )
