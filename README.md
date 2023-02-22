2945ae5b4bc7ac05b88fb05b80be73a160d1c8b5
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-22 14:58:19 +0530

    Modified README and Hero Section

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # eventOS
+
+**** Attributions - 
+- Pixabay for [Hero Image](https://pixabay.com/photos/meeting-addis-ababa-ethiopia-hall-83519/)
--- eventos-frontend/src/components/Hero.jsx
+++ eventos-frontend/src/components/Hero.jsx
@@ -1,11 +1,13 @@
 
 const Hero = ()  => {
     return (
-        <div className="w-full h-[500px] bg-red-500 text-white" id="hero">
-            <h1>Welcome to EventOS</h1>
-            <p>WHAT'S OPEN SOURCE INDIA?</p>
-            <p>Open Source India (OSI) is an attempt by the team at Open Source For You magazine in partnership with India’s Open Source community and industry—to bring various stake-holders under one roof.</p>
-            <p>The mission of OSI is simple—to accelerate development and deployment of Open Source in India, and beyond.</p>
+        <div className="w-full h-[500px]" id="hero">
+            <div className="text-white flex flex-col text-center justify-center pt-40 mx-auto w-[800px]">
+                <h1 className="text-4xl">Welcome to EventOS</h1>
+                <p>WHAT'S OPEN SOURCE INDIA?</p>
+                <p>Open Source India (OSI) is an attempt by the team at Open Source For You magazine in partnership with India’s Open Source community and industry—to bring various stake-holders under one roof.</p>
+                <p>The mission of OSI is simple—to accelerate development and deployment of Open Source in India, and beyond.</p>
+            </div>            
         </div>
     )
 }
