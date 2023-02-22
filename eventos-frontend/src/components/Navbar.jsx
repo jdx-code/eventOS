0d397de363d16a691c81a8fd51a6c2c4dfdaabb3
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-22 14:42:06 +0530

    Working on Navbar and Hero Section

--- eventos-frontend/src/components/Navbar.jsx
+++ eventos-frontend/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 const Navbar = () => {
     return (
-        <div className="flex justify-between items-center w-full h-20 bg-slate-900 text-blue-100">
-            <h3>Developer's Events</h3>
-            <ul className="flex">
+        <div className="flex justify-around items-center w-full h-20 bg-slate-900 text-blue-100">
+            <h3>EventOS</h3>
+            <ul className="flex border-2 border-yellow-300 ml-28">
                 <li className="px-10">About</li>
                 <li className="px-10">Events</li>
                 <li className="px-10">Info</li>
                 <li className="px-10">Contact</li>
             </ul>
+            <div>
+                <button>Login</button>
+                <button>Become a member</button>
+            </div>
+            
         </div>
     )
 }
