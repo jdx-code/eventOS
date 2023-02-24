e456a573c4a19dc762c4703a6e4dfa5cafa20560
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-24 15:58:28 +0530

    Installed express, mongo and cors packages

--- eventos-frontend/src/components/Hero.jsx
+++ eventos-frontend/src/components/Hero.jsx
@@ -2,7 +2,7 @@
 const Hero = ()  => {
     return (
         <div className="w-full h-[500px]" id="hero">
-            <div className="text-white flex flex-col text-center justify-center pt-40 mx-auto w-[800px]">
+            <div className="text-white flex flex-col text-center justify-center pt-40 mx-auto w-[800px] border-2 border-yellow-300">
                 <h1 className="text-4xl">Welcome to EventOS</h1>
                 <p>WHAT IS EVENTOS?</p>
                 <p>EventOS is a platform to view, book and monitor events related to Computer Science and Engineering discipline that are held by various organisations throughout the year.</p>
--- eventos-frontend/src/components/Navbar.jsx
+++ eventos-frontend/src/components/Navbar.jsx
@@ -2,15 +2,16 @@ const Navbar = () => {
     return (
         <div className="flex justify-around items-center w-full h-20 bg-slate-900 text-blue-100">
             <h3>EventOS</h3>
-            <ul className="flex border-2 border-yellow-300 ml-28">
-                <li className="px-10">About</li>
-                <li className="px-10">Events</li>
-                <li className="px-10">Info</li>
-                <li className="px-10">Contact</li>
+            <ul className="flex border-2 border-yellow-300">
+                <li className="px-12">About</li>
+                <li className="px-12">Events</li>
+                <li className="px-12">Info</li>
+                <li className="px-12">Contact</li>
+                <li className="px-12">Community</li>
             </ul>
             <div>
-                <button>Login</button>
-                <button>Become a member</button>
+                <button className="pr-4">Login</button>
+                <button>Sign up</button>
             </div>
             
         </div>
