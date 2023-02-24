4e3a84d3457b599250a3d46ca5449f4dd6d76889
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-02-24 20:05:03 +0530

    imported mongoose and added database connection string

--- eventos-backend/index.js
+++ eventos-backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false');
 
 app.listen(3001, ()=>{
     console.log('Server connected..');
